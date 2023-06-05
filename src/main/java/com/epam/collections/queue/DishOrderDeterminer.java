4905e8e4a7c8e1dd66c64a6be96c4ec90fd59363
Artsiom Pashun <a.pashun@syberry.com>
2023-06-05 13:14:41 +0200

    Finished module 4, task 4

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,29 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> orderList = new ArrayList<>();
+        LinkedList<Integer> dishList = new LinkedList<>();
+        while (numberOfDishes > 0) {
+            dishList.add(numberOfDishes);
+            numberOfDishes--;
+        }
+        dishList = dishList.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toCollection(LinkedList::new));
+        int i = 1;
+        while (!dishList.isEmpty()) {
+            int dishNumber = dishList.poll();
+            if (i % everyDishNumberToEat == 0) {
+                orderList.add(dishNumber);
+            } else {
+                dishList.add(dishNumber);
+            }
+            i++;
+        }
+        return orderList;
     }
 }
